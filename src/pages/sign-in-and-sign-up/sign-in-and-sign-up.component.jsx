a42aabf85b6c616891da2262b73e67a0e5bcb323
Hugo <hugo.berthome@epitech.eu>
2021-02-14 15:49:33 +0900

    Create signup and implement db from firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Signin from "../../components/sign-in/sign-in.component";
+import Signup from "../../components/sign-up/sign-up.component";
 
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <Signin />
+        <Signup />
     </div>
 );
 
